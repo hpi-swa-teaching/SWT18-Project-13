c092c0f9705cce479e4ba1c7520590f91f3a4909
lassesteffen <lasse.steffen@t-online.de>
2018-05-22 15:07:08 +0200

    copyfonts.sh updated [skip ci]

--- build-support/copyfonts.sh
+++ build-support/copyfonts.sh
@@ -1,3 +1,3 @@
-mkdir -p /home/travis/smalltalkCI-master/_builds/Scamper/fonts
-cp -R $TRAVIS_BUILD_DIR/build-support/fonts/* /home/travis/smalltalkCI-master/_builds/Scamper/fonts
-chmod -R 777 /home/travis/smalltalkCI-master/_builds/Scamper/fonts/*
+mkdir -p /home/travis/smalltalkCI-master/_builds/Roboto/
+cp -R $TRAVIS_BUILD_DIR/build-support/fonts/* /home/travis/smalltalkCI-master/_builds/Roboto/
+chmod -R 777 /home/travis/smalltalkCI-master/_builds/Roboto/*
