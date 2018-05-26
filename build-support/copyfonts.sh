5d3979951289814fe1499c4bff4e9ef5499d166b
mschroederi <me@matspoerschke.de>
2018-05-26 17:19:59 +0200

    Update copyfonts.sh

--- build-support/copyfonts.sh
+++ build-support/copyfonts.sh
@@ -1,13 +1,13 @@
 echo $TRAVIS_OS_NAME
 
-PATH="/home/travis/smalltalkCI-master/_builds/Roboto/"
+IMAGE_PATH="/home/travis/smalltalkCI-master/_builds/Roboto/"
 
-if [ "$TRAVIS_OS_NAME" == "osx" ]
-  then PATH="/Users/travis/smalltalkCI-master/_builds/Roboto/"
-fi
+if [ "$TRAVIS_OS_NAME" == "osx" ];
+  then IMAGE_PATH="/Users/travis/smalltalkCI-master/_builds/Roboto/";
+fi;
 
-mkdir -p $PATH
-cp -R $TRAVIS_BUILD_DIR/build-support/fonts/* $PATH
-chmod -R 777 $PATH
+mkdir -p $IMAGE_PATH
+cp -R $TRAVIS_BUILD_DIR/build-support/fonts/* $IMAGE_PATH
+chmod -R 777 $IMAGE_PATH
 
-ls -lisa $PATH
+ls -lisa $IMAGE_PATH
