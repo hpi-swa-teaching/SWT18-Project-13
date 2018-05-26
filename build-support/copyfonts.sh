9076ddb03327363eff6090110bc273085e3e5b4c
mschroederi <me@matspoerschke.de>
2018-05-26 16:41:39 +0200

    Update copyfonts.sh

--- build-support/copyfonts.sh
+++ build-support/copyfonts.sh
@@ -1,4 +1,4 @@
-ls -R / | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/   /' -e 's/-/|/'
-mkdir -p /home/travis/smalltalkCI-master/_builds/Roboto/
+find / -name 'smalltalkCI-master'
+mkdir -r -p /home/travis/smalltalkCI-master/_builds/Roboto/
 cp -R $TRAVIS_BUILD_DIR/build-support/fonts/* /home/travis/smalltalkCI-master/_builds/Roboto/
 chmod -R 777 /home/travis/smalltalkCI-master/_builds/Roboto/*
