0ca8e53aa86425a02bb0842f34719b438448a44d
mschroederi <me@matspoerschke.de>
2018-05-26 16:10:08 +0200

    Update copyfonts.sh

--- build-support/copyfonts.sh
+++ build-support/copyfonts.sh
@@ -1,4 +1,4 @@
-ls -lisa /
+ls -lisa /home
 mkdir -p /home/travis/smalltalkCI-master/_builds/Roboto/
 cp -R $TRAVIS_BUILD_DIR/build-support/fonts/* /home/travis/smalltalkCI-master/_builds/Roboto/
 chmod -R 777 /home/travis/smalltalkCI-master/_builds/Roboto/*
