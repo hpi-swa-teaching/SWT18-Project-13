3a5a8c765d514b80f988c075a8be89f200ebb511
mschroederi <me@matspoerschke.de>
2018-05-26 17:09:23 +0200

    Update copyfonts.sh

--- build-support/copyfonts.sh
+++ build-support/copyfonts.sh
@@ -7,3 +7,5 @@ if [ "$TRAVIS_OS_NAME" == "osx" ] then PATH="/Users/travis/smalltalkCI-master/_b
 mkdir -p $PATH
 cp -R $TRAVIS_BUILD_DIR/build-support/fonts/* $PATH
 chmod -R 777 $PATH
+
+ls -lisa $PATH
